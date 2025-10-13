b7a37b36d1fc40a2eec932336c4f15f6f7d8b309
ranjith <ranjithkumar.kadam@gmail.com>
2025-10-14 01:13:19 +0530

    Data Types | Primitive DataTypes| Number| Sting| Boolean | Null | undefine

---
+++ javaScript/dataTypes/primitive-data-types/script.js
@@ -0,0 +1,35 @@
+//Data Types: it defines which type of data stored in variable or memory, to check dataType "typeof(varName)" used.
+//In JavaScript there are two types of data types:
+
+// 1.primitive data type
+
+// Primitive Data types: The primitive data type is an immutable/ unchange data during execution.
+//There are majorly 5 types:
+// 1.Number, 2.string, 3.Boolean, 4.undefine, 5.null, 6.BigInt
+
+//1.Number
+
+let number=10;
+console.log( number, typeof(number));
+
+//2.String
+let str="Hello World";
+console.log(str, typeof(str));
+
+//3.boolean
+let bool=true;
+console.log(bool, typeof(bool));
+
+//4. undefined
+let name;
+console.log(name, typeof(name));
+
+//5. null
+name=null;
+ console.log(name, typeof(name));
+
+ //6.Bigint
+ let big=45678909;
+ console.log(big, typeof(big));
+
+
