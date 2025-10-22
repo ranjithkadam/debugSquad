6a8bdb0fb0c21a411dbf0bfa97005338a027dde6
ranjith <ranjithkumar.kadam@gmail.com>
2025-10-22 12:01:36 +0530

    practise

---
+++ javaScript/operators/practise.js
@@ -0,0 +1,30 @@
+console.log("True"==1); // false
+console.log("true"==1); //false
+console.log(true == 1); //true
+console.log("abc"-12);  //NaN
+console.log(NaN==NaN);  //false
+
+let a=10;
+a
+function add(){
+    // a=120;
+    console.log(a);     // 10
+    a=20                       
+    console.log(a)      // 20
+}
+add();
+
+let person={
+    name:"RadhaKrishna",
+    rollNo:123,
+    marks:90,
+ }
+
+ dataType= typeof(person);
+//  console.log(typeof(object))
+//  console.log(dataType === object);
+
+
+console.log(true==1 && false==0); //true && true == true
+console.log(typeof{});
+console.log("apple"+true)   //appletrue
