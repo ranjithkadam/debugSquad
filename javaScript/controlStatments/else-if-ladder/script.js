c84ad966140b9c8ca526f29b2403be0aefbc2c05
ranjith <ranjithkumar.kadam@gmail.com>
2025-10-23 13:28:24 +0530

    else if ladder

--- javaScript/controlStatments/else-if-ladder/script.js
+++ javaScript/controlStatments/else-if-ladder/script.js
@@ -1,12 +1,40 @@
+// $$ "else-if" ladder is a control statement used to control flow of execution of program by checking conditions.
+// $$ The "else-if" ladder has used to checking multiple conditions where the conditions is True then That Block will Execute.
+// $$ In this one "if" statement and multiple "else if" statements and last else Statement.
 
-// $$ "if()" statement is a control statement used to control flow of execution of a program. 
-// - by checking condition.
-  
-// $$ "If()"" block will execute only when the condition is True.
 
-// Syntax: if(Condition){ ....code... }
 
-let num1=20;
-if(num1 == 20){
-    console.log("If Block Executed");
+// checking the Grade
+let marks=parseInt(prompt("Enter Your Marks: "));
+if((marks>=36) && (marks<40)){
+    console.log("Passed");
+}
+
+else if((marks>= 40) && (marks<65)){
+    console.log(" Grade B ");
+}
+
+else if((marks>=66) && (marks<75)){
+    console.log("Grade B+");
+}
+
+else if((marks>=75) && (marks<90)){
+    console.log("Grade A");
+}
+
+else if((marks>=90) && (marks<100)){
+ console.log(" Grade A+ #topper");
+}
+
+else if(marks>100){
+    console.log("Sorry Marks are Invalid")
+}
+
+    else if(marks<0){
+        console.log("Sorry Marks are Invalid");
+    }
+
+
+else{
+    console.log("Fail..., All best for Next Exam")
 }
