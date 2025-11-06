0c3e2d596c8fe1d368827a614cc9c4dbe92f1a56
ranjith <ranjithkumar.kadam@gmail.com>
2025-11-06 13:20:15 +0530

    task updated

--- javaScript/Assignment_On_loops/task-26/script.js
+++ javaScript/Assignment_On_loops/task-26/script.js
@@ -87,8 +87,27 @@ cards.innerHTML+=`
     `;
     }
 
-setInterval(
-    function(){
-        let a=new Date();
-        document.getElementById("header2").innerText= a.toLocaleTimeString();
-    },1000);
+  const offerStartTime = new Date();
+offerStartTime.setDate(offerStartTime.getDate() + 3);
+
+function updateTimer() {
+   
+  const now = new Date();
+  const diff = offerStartTime - now;
+
+  if (diff <= 0) {
+     
+    clearInterval(timer);
+    
+  }
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
+
+  document.getElementById("timer").innerText=`${days}d : ${hours}h : ${minutes}m : ${seconds}s`;
+}
+
+
+const timer = setInterval(updateTimer, 1000);
