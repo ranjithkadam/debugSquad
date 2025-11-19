cac59cc91ea36acfa213b55a4e2c19efc37890d7
ranjith <ranjithkumar.kadam@gmail.com>
2025-11-19 18:49:27 +0530

    productCart Updated

---
+++ javaScript/productCart/app.js
@@ -0,0 +1,33 @@
+function registraion() {
+  let username = document.getElementById("name").value;
+  let password = document.getElementById("password").value;
+  let email = document.getElementById("email").value;
+
+  if(!username || !password){
+   alert("Fill the Fields..");
+   return;
+  }
+   
+    localStorage.setItem("name", username.trim());
+    localStorage.setItem("email", email.trim());
+    localStorage.setItem("password", password.trim());
+    alert("Registered Successfully");
+}
+
+function login() {
+  let username = document.getElementById("name").value;
+  //let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  if (username === localStorage.getItem("name") && password === localStorage.getItem("password")) {
+    alert("LogIn Successfully.."); 
+    window.location.href = "./index.html";
+    // document.getElementById("greet").submit();
+  }
+
+  else {
+    alert("Invalid username or Password !");
+  }
+};
+
+ 
