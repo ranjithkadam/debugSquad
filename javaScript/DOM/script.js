32ffff2d9a458137de6542ad1c77cb6ebd7bec98
ranjith <ranjithkumar.kadam@gmail.com>
2025-11-24 13:49:43 +0530

    DOM | selector | Manupulation

---
+++ javaScript/DOM/script.js
@@ -0,0 +1,31 @@
+/*
+DOM Opertaions 
+*/ 
+// DOM Selectors
+// 1. getElementByID("IDName")
+// let title= document.getElementById("title");
+// title.style.backgroundColor="red";
+// title.style.color="white";
+
+//2. getElementsByClassName("Classname")
+
+// let title=document.getElementsByClassName("title-2");
+
+// for(let i=0; i<title.length; i++){
+// title[i].style.backgroundColor="green";
+// }
+
+// title[0].style.backgroundColor="pink";
+
+//3.getElementsByTagName("tagname")
+// let title=document.getElementsByTagName("h1");
+// title[0].style.backgroundColor="pink";
+
+//DOM Manupulation
+
+//1.createElement
+
+let title=document.createElement("h2");
+title.innerText="Hello World I from JS";
+document.body.appendChild(title);
+document.body.remove(title);
