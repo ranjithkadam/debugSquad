2e74f9d2e98a145b702708f2a7ae57c4e7f27a51
ranjith <ranjithkumar.kadam@gmail.com>
2025-11-24 18:34:08 +0530

    DOM | Monupulation

--- javaScript/DOM/script.js
+++ javaScript/DOM/script.js
@@ -21,11 +21,23 @@ DOM Opertaions
 // let title=document.getElementsByTagName("h1");
 // title[0].style.backgroundColor="pink";
 
+// 4.querySelector():
+
+    // let title=document.querySelector("#title");
+    // title.style.backgroundColor="red";
+
+// 5.querySelectorAll():
+
+    let title=document.querySelectorAll(".title-2");
+    title.style.backgroundColor="green";
+
 //DOM Manupulation
 
 //1.createElement
 
-let title=document.createElement("h2");
-title.innerText="Hello World I from JS";
-document.body.appendChild(title);
-document.body.remove(title);
+// let title=document.createElement("h2");
+// title.innerText="Hello World I from JS";
+// document.body.appendChild(title);
+// document.body.remove(title);
+
+
