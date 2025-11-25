0ec8beed141e31abc5b84844d0370655e705124a
ranjith <ranjithkumar.kadam@gmail.com>
2025-11-25 11:50:08 +0530

    DOM | Attributes | classList

--- javaScript/DOM/script.js
+++ javaScript/DOM/script.js
@@ -28,16 +28,50 @@ DOM Opertaions
 
 // 5.querySelectorAll():
 
-    let title=document.querySelectorAll(".title-2");
-    title.style.backgroundColor="green";
+    // let title=document.querySelectorAll(".title-2");
+    // title[1].style.backgroundColor="green";
 
 //DOM Manupulation
 
 //1.createElement
 
-// let title=document.createElement("h2");
+// let title=document.createElement("div");
 // title.innerText="Hello World I from JS";
 // document.body.appendChild(title);
-// document.body.remove(title);
+
+// 2. remove()
+//  document.body.remove(title);
 
 
+//DOM Atrributes
+
+// getAttribute("attributeName")
+let title=document.getElementById("img");
+console.log(title.getAttribute("src"));
+
+//  setAttribute("attribte", "values")
+console.log(title.setAttribute("alt","img"));
+
+// hasAttribute("attributeName")       returns true or false values
+console.log(title.hasAttribute("src"));
+
+// DOM ClassList
+// .classList.add("className")
+title.classList.add("image");
+
+// .classList.remove("className")
+title.classList.remove("image");
+
+// .classList.toggle(image2)
+
+title.classList.toggle("image");
+
+let body = document.getElementById("body");
+function theme(){
+// body.classList.toggle(body.style.background="white");
+body.classList.toggle(body.style.background="black");
+body.classList.toggle(body.style.color="white");
+
+// body.classList.toggle(body.style.background="white");
+
+}
