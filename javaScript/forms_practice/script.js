58cd5f757a267e4a0f9a174aacec4f983957766b
ranjith <ranjithkumar.kadam@gmail.com>
2025-12-01 11:49:26 +0530

    preventDefault mehod Practiced

--- javaScript/forms_practice/script.js
+++ javaScript/forms_practice/script.js
@@ -1,11 +1,11 @@
  
-// let form = document.getElementById("form");
+ let form = document.getElementById("form");
 form.addEventListener("submit", function (event) {
     let username=document.getElementById("username").value;
     let password=document.getElementById("password").value;
     event.preventDefault();
     if (username != "" && password != "" ) {
-        msg.innerText = `${(username.value + 1).length}/100`;
+        // msg.innerText = `${(username.value + 1).length}/100`;
         alert("submitted");
         
     }
@@ -14,3 +14,11 @@ form.addEventListener("submit", function (event) {
     }
      
 });
+
+document.getElementById("link").addEventListener("click",
+    function(event){
+        event.preventDefault();
+    }
+);
+
+ 
