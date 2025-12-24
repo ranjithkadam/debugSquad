707c8042fdeb83087f94d773a304d23e43052ad3
ranjith <ranjithkumar.kadam@gmail.com>
2025-12-25 00:26:04 +0530

    show carts task completed

--- javaScript/fetch_Function/task/script.js
+++ javaScript/fetch_Function/task/script.js
@@ -1,15 +1,19 @@
 
-// const input=document.getElementById("input").value;
-fetch("https://dummyjson.com/products")
-.then( contant => contant.json())
-.then(data => displayCarts(data.products) /*console.log(data)*/)
-.catch(error => console.log("unable to fetch the api"))
-const container = document.getElementById("container");
+function searchcarts() {
+   const input = document.getElementById("input").value.toLowerCase();
+   fetch(`https://dummyjson.com/products/search?q=` + input)
+      .then(contant => contant.json())
+      .then(data => displayCarts(data.products) /*console.log(data)*/)
+      .catch(error => console.log("unable to fetch the api"))
+}
+searchcarts();
 
 
-function displayCarts(data){
- data.forEach(value =>{
-    container.innerHTML+=`
+function displayCarts(data) {
+   const container = document.getElementById("container");
+   container.innerHTML = " ";
+   data.forEach(value => {
+   container.innerHTML += `
     <div id="allCarts${value.id}" class="h-70 w-100 py-3 border-stone-500 m-2 p-2 flex-shrink-1 rounded bg-stone-200 shadow flex justify-space-between align-center hover:translate-y-1 transition-all">
     <img src="${value.images}" alt="No Img" class="bg-white rounded w-30 ">
     <div class="ml-5"> 
@@ -20,5 +24,5 @@ function displayCarts(data){
     </div>
     </div>
     `;
- })
+   })
 }
