e49152aa7b8347f43faafdf372471c26549410b5
ranjith <ranjithkumar.kadam@gmail.com>
2025-12-27 13:57:25 +0530

    spread methods

---
+++ javaScript/spread_method/script.js
@@ -0,0 +1,26 @@
+// spread method used to seperate the array value into individual values.
+
+let arr=[2,3,4,4,5, "ranjith"];
+// console.log(...arr);
+
+//=========================================
+
+function example(...nums){
+    // console.log(...nums);
+    // console.log(nums[2]);
+}
+
+example(12,23,34)
+ //=========================================
+
+// swapping two numbers using spread method.
+
+let a=10;
+let b=20;
+let oldarr=[a,b];
+
+// console.log(...oldarr);
+ [b, a]= oldarr; 
+//  console.log(a,b);
+
+ //=========================================
